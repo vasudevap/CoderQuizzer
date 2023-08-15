c7ad3658b6e509162d374708ee250bb191f5d6f8
Prashant <prashant.vasudeva@gmail.com>
2023-08-15 18:58:09 -0400

    able to catch the selected option

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,27 +7,54 @@ var mainSubEl = document.getElementById("mainSubMessage");
 var optionsListEl = document.getElementById("answerOptions");
 
 var secondsLeft = 0;
+var correctAnswers = 0;
+var wrongAnswers = 0;
+var PlayerName = "";
 
-function startQuiz() {
-  // Sets interval in variable
-  var timerInterval = setInterval(function() {
-    timeEl.textContent = "Time left: "+secondsLeft;
+init();
 
-    if(secondsLeft === 0) {
-        // Stops execution of action at set interval
-        clearInterval(timerInterval);
-        // Game over, call function to do scores
-        showScores();
-    }
+function init() {
+    
+  var startBtn = document.createElement("button");
+  startBtn.setAttribute("Id","startButton");
+  startBtn.textContent = "Start Quiz";
 
-    secondsLeft--;
-
-    }, 1000);
+  document.getElementById("mainMessage").textContent="Coding Quiz Challenge";
+  document.getElementById("mainSubMessage").textContent="Try to answer the following code related questions within the time limit.  Keep in mind that the incorrect answers will penalize your scoretime by ten seconds!";
+  
 }
 
 optionsListEl.addEventListener("click", function(event) {
-  console.log(event.target);
-})
+
+  event.preventDefault();
+  var optionSelected = event.target;
+  console.log(optionSelected.textContent);
+  // DEBUG:
+  // console.log(optionSelected);
+
+  // if selected option is the correct one
+  // add to correctAnswers
+  // otherwise, add to wrongAnswers and set
+  // secondsLeft - 10 (player loses 10sec)
+  //
+  switch (optionSelected.getAttribute("id")) {
+    case "option1":
+      console.log("option1");
+      // if questionAsked.answer = option1
+      //    correctAnswers++
+      // else 
+      //    incorrectAnswers++
+      //
+    case "option2":
+      console.log("option2");
+    case "option3":
+      console.log("option3");
+    case "option4":
+      console.log("option3");
+  }
+  }
+)
+
 startButtonEl.addEventListener("click", function(event) {
     secondsLeft=3;
     formatScreenForQuiz();
@@ -36,11 +63,12 @@ startButtonEl.addEventListener("click", function(event) {
 // Function to create and append colorsplosion image
 function getScoring() {
   timeEl.textContent = " ";
-//   var pEl = document.createElement("p");
-//   pEl.textContent = "Game Over - here is your score";
+  //   var pEl = document.createElement("p");
+  //   pEl.textContent = "Game Over - here is your score";
   mainEl.textContent="Game Over - here is your score";
-//   mainEl.appendChild(pEl);
+  //   mainEl.appendChild(pEl);
 
+  
 }
 
 function formatScreenForQuiz() {
@@ -69,7 +97,7 @@ function showQuestions (){
   var optionB = document.createElement("li");
   var optionC = document.createElement("li");
   var optionD = document.createElement("li");
-  
+
   questionEl.textContent = "This is question 1";
 
   descEl.textContent = "";
@@ -96,12 +124,19 @@ function showQuestions (){
 
 }
 
-function init() {
-    mainSubEl.textContent = "Try to answer the following code related questions within the time limit.  Keep in mind that the incorrect answers will penalize your scoretime by ten seconds!";
-    startButtonEl.textContent = "Start Quiz";
-    mainEl.textContent="Coding Quiz Challenge";
+function startQuiz() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    timeEl.textContent = "Time left: "+secondsLeft;
 
+    if(secondsLeft <= 0) {
+        // Stops execution of action at set interval
+        clearInterval(timerInterval);
+        // Game over, call function to do scores
+        showScores();
+    }
+
+    secondsLeft--;
+
+    }, 1000);
 }
-
-init();
-
